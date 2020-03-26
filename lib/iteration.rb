76e49579d4596499d6952570093be8784d4636e1
Mack McQuen <mackmcquen@comcast.net>
2020-03-26 05:44:57 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -15,7 +15,8 @@ def join_ingredients(src)
     puts spice_rack[row_index][element_index]
     element_index += 1
 end
-
+ row_index += 1
+end
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
