a83a80ed1740a6fcc41a8a15adba0aa1c647e280
Mack McQuen <mackmcquen@comcast.net>
2020-03-26 05:46:58 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -12,7 +12,7 @@ def join_ingredients(src)
     element_index = 0
     topping1 = spice_rack[row_index][0]
     topping2 = spice_rack[row_index][1]
-  ingredient_array.push
+  ingredient_array.push("I love")
 end
 
 def find_greater_pair(src)
