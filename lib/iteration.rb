55a54d74202afd03a0180825417e8aeef5dc1fc1
Mack McQuen <mackmcquen@comcast.net>
2020-03-26 06:03:17 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,7 +24,7 @@ def find_greater_pair(src)
   # that are in the inner Arrays
   larger_number = Array.new
    row_index = 0
-   while row_index < 
+   while row_index < src.count 
 end
 
 def total_even_pairs(src)
